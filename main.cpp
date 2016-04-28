1c7b1756f6dd8d0b993c0a5ca3e6df578d39b1a2
Jie Wang <jiewang@nat-oitwireless-inside-vapornet100-b-6814.Princeton.EDU>
2016-04-28 10:50:22 -0400

    minor change

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 int main(){
 	int NPhi,invNu,nWarmup,nMeas,nSteps,nBins,seed;
 	ifstream infile("params");
-	infile>>NPhi>>invNu;
+	infile>>NPhi>>invNu; 
 	infile>>nWarmup>>nMeas>>nSteps>>nBins;
 	infile>>seed;
 	//initialize MC object
@@ -22,73 +22,73 @@ int main(){
     void coul_energy_dbar(LATTICE& edbar, double&, int nWarmup, int nMeas, int nSteps, int nBins, double* dbar_parameter );
     
     
-//    for (int i=0; i<5; i++) {
-//        for (int j=0; j<5; j++) {
-////            cout<<"\ndbar_parameter = ("<<i<<", "<<j<<")/NPhi"<<endl;
-//			dbar_parameter[0]=0.2*i;
-//			dbar_parameter[1]=0.2*j;
-//            coul_energy_dbar(ll,ave_E,nWarmup,nMeas,nSteps,nBins,dbar_parameter);
-////            cout<<"coulomb energy = "<<ave_E<<endl;
-//            out_co_dbar<<i<<"   "<<j<<"   "<<ave_E<<endl;
-//        }
-//    }
-    
-    
- //   ll.change_dbar_parameter(0.2,0.4);
-	for(int s=0;s<nBins;s++){
-    
-		ll.reset();
-		ll.step(nWarmup);
-		double E=0,E2=0,P=0,P2=0,three=0;
-		double e,p;
-		deque<double> e_tracker, p_tracker;
-		int Ntrack=10;
-		vector<double> autocorr_e(Ntrack,0), autocorr_p(Ntrack,0);
-		for(int i=0;i<nMeas;i++){
-	//		cout<<ll.get_weight(ll.get_locs())<<" "<<norm(ll.get_wf(ll.get_locs(),ll.get_ds()))*exp(-ll.Ne*ll.Ne)<<endl;
-			ll.step(nSteps);
-			e=ll.coulomb_energy();
-			E+=e;
-			E2+=e*e;
-//			p=log(ll.running_weight);
-//			P+=p;
-//			P2+=p*p;
-//			three+=ll.threebody();
-			eout<<e<<endl;
-//			//autocorrelations
-//			e_tracker.push_front(e);
-//			p_tracker.push_front(p);
-//			if(i>=Ntrack){
-//				for(int j=0;j<Ntrack;j++){
-//					autocorr_e[j]+=e_tracker[j]*e;
-//					autocorr_p[j]+=p_tracker[j]*p;
-//				}
-//				e_tracker.pop_back();
-//				p_tracker.pop_back();
-//			}
-			
-			ll.update_structure_factors();
-		}
-//		outfile<<E/(1.*nMeas*ll.Ne)<<" "<<(E2/(1.*nMeas)-pow(E/(1.*nMeas),2))/(1.*ll.Ne)<<" "<<three/(1.*nMeas)<<endl;
-        outfile<<E/(1.*nMeas*ll.Ne)<<" "<<(E2/(1.*nMeas)-pow(E/(1.*nMeas),2))/(1.*ll.Ne)<<endl;
-		cout<<"acceptance rate: "<<(1.*ll.accepts)/(1.*ll.tries)<<endl;
-	//	cout<<"almost done"<<endl;
-	
-//		ofstream auto_out("auto");
-//		for(int j=0;j<Ntrack;j++){
-//			auto_out<<j+1<<" ";
-//			auto_out<<autocorr_e[j]/(1.*(nMeas-Ntrack))<<" "<<pow(E/(1.*nMeas),2)<<" "<<(E2/(1.*nMeas)-pow(E/(1.*nMeas),2))<<" ";
-//			auto_out<<autocorr_p[j]/(1.*(nMeas-Ntrack))<<" "<<pow(P/(1.*nMeas),2)<<" "<<(P2/(1.*nMeas)-pow(P/(1.*nMeas),2))<<" ";
-//			auto_out<<endl;
+    for (int i=0; i<5; i++) {
+        for (int j=0; j<5; j++) {
+//            cout<<"\ndbar_parameter = ("<<i<<", "<<j<<")/NPhi"<<endl;
+			dbar_parameter[0]=0.2*i;
+			dbar_parameter[1]=0.2*j;
+            coul_energy_dbar(ll,ave_E,nWarmup,nMeas,nSteps,nBins,dbar_parameter);
+//            cout<<"coulomb energy = "<<ave_E<<endl;
+            out_co_dbar<<i<<"   "<<j<<"   "<<ave_E<<endl;
+        }
+    }
+//    
+//    
+// //   ll.change_dbar_parameter(0.2,0.4);
+//	for(int s=0;s<nBins;s++){
+//    
+//		ll.reset();
+//		ll.step(nWarmup);
+//		double E=0,E2=0,P=0,P2=0,three=0;
+//		double e,p;
+//		deque<double> e_tracker, p_tracker;
+//		int Ntrack=10;
+//		vector<double> autocorr_e(Ntrack,0), autocorr_p(Ntrack,0);
+//		for(int i=0;i<nMeas;i++){
+//	//		cout<<ll.get_weight(ll.get_locs())<<" "<<norm(ll.get_wf(ll.get_locs(),ll.get_ds()))*exp(-ll.Ne*ll.Ne)<<endl;
+//			ll.step(nSteps);
+//			e=ll.coulomb_energy();
+//			E+=e;
+//			E2+=e*e;
+////			p=log(ll.running_weight);
+////			P+=p;
+////			P2+=p*p;
+////			three+=ll.threebody();
+//			eout<<e<<endl;
+////			//autocorrelations
+////			e_tracker.push_front(e);
+////			p_tracker.push_front(p);
+////			if(i>=Ntrack){
+////				for(int j=0;j<Ntrack;j++){
+////					autocorr_e[j]+=e_tracker[j]*e;
+////					autocorr_p[j]+=p_tracker[j]*p;
+////				}
+////				e_tracker.pop_back();
+////				p_tracker.pop_back();
+////			}
+//			
+//			ll.update_structure_factors();
 //		}
-	}
-    outfile<<endl;
-    
-    
-//	cout<<"start print"<<endl;
-//	ll.print_structure_factors(nMeas*nBins);
-	eout.close();
-	outfile.close();
+////		outfile<<E/(1.*nMeas*ll.Ne)<<" "<<(E2/(1.*nMeas)-pow(E/(1.*nMeas),2))/(1.*ll.Ne)<<" "<<three/(1.*nMeas)<<endl;
+//        outfile<<E/(1.*nMeas*ll.Ne)<<" "<<(E2/(1.*nMeas)-pow(E/(1.*nMeas),2))/(1.*ll.Ne)<<endl;
+//		cout<<"acceptance rate: "<<(1.*ll.accepts)/(1.*ll.tries)<<endl;
+//	//	cout<<"almost done"<<endl;
+//	
+////		ofstream auto_out("auto");
+////		for(int j=0;j<Ntrack;j++){
+////			auto_out<<j+1<<" ";
+////			auto_out<<autocorr_e[j]/(1.*(nMeas-Ntrack))<<" "<<pow(E/(1.*nMeas),2)<<" "<<(E2/(1.*nMeas)-pow(E/(1.*nMeas),2))<<" ";
+////			auto_out<<autocorr_p[j]/(1.*(nMeas-Ntrack))<<" "<<pow(P/(1.*nMeas),2)<<" "<<(P2/(1.*nMeas)-pow(P/(1.*nMeas),2))<<" ";
+////			auto_out<<endl;
+////		}
+//	}
+//    outfile<<endl;
+//    
+//    
+////	cout<<"start print"<<endl;
+////	ll.print_structure_factors(nMeas*nBins);
+//	eout.close();
+//	outfile.close();
    
 }
 
